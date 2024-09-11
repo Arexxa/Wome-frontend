96b399578d8d8afe701fbb4f2f8fefc660ff5540
Arexxa <shahrulayol31@gmail.com>
2024-09-11 15:45:29 +0800

    master

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,16 +10,28 @@ import Footer from "../../components/Footer";
 
 function Home() {
   return (
-    <div className="flex flex-col">
+    // <div className="flex flex-col">
      
-      <WomeGlobeSection />
-      <WomeEligibilitySection />
-      <WomeMissionSection />
-      <WomeBenefitSection />
-      <WomeTeamSection />
-      <div className='desktop-bg-roadmap-footer'>
-        <WomeRoadmapSection />
-        <Footer />
+    //   <WomeGlobeSection />
+    //   <WomeEligibilitySection />
+    //   <WomeMissionSection />
+    //   <WomeBenefitSection />
+    //   <WomeTeamSection />
+    //   <div className='desktop-bg-roadmap-footer'>
+    //     <WomeRoadmapSection />
+    //     <Footer />
+    //   </div>
+    // </div>
+    <div className="flex flex-col items-center h-[100vh]">
+      <div className="flex flex-col items-center justify-center mt-10 h-full gap-10">
+        <div className="text-6xl">404</div>
+        <div className="text-4xl">Page Not Found!</div>
+        <a
+          href="/"
+          className="px-6 py-3 rounded-md text-white hover:shadow-xl bg-primary"
+        >
+          Go Home
+        </a>
       </div>
     </div>
   );
